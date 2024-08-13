026f478750e8e0456e1b26ce9e136d2224735f92
Karan <karanlalwani.me@gmail.com>
2024-08-13 19:07:45 +0530

    Delete frontend directory

--- frontend/src/conf.js
+++
@@ -1,8 +0,0 @@
-const conf = {
-    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    geminiApiUrl: String(import.meta.env.VITE_GEMINI_API_URL),
-}
-export default conf
